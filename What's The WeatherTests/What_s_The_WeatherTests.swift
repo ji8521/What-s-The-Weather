b4ecb7e30220578ffaa37227bb4c8339976de30c
Jeffrey Isaray <ji8521@gmail.com>
2015-10-14 09:58:09 -0400

    Initial Commit

---
+++ What's The WeatherTests/What_s_The_WeatherTests.swift
@@ -0,0 +1,36 @@
+//
+//  What_s_The_WeatherTests.swift
+//  What's The WeatherTests
+//
+//  Created by Jeffrey Isaray on 10/14/15.
+//  Copyright © 2015 Jeffrey Isaray. All rights reserved.
+//
+
+import XCTest
+@testable import What_s_The_Weather
+
+class What_s_The_WeatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
